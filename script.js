3ec9caabc0ac42f440ff971a29706e859d048286
Varada M <58105997+varada1110@users.noreply.github.com>
2020-07-30 12:27:06 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+
+    function myFunction()
+        {
+            var image=document.getElementById("bulb-off");
+            if(image.src.match("LC"))
+                {
+                image.src="OIPXWBOY9YU.jpg";
+                document.inner
+                }
+            else
+                {
+                image.src="LC.jpg";
+                }
+        
+        }
+        
+    
